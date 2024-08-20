bcfa755f7c8e4e161ae622dd6056046cf55abe4f
Chase <brendanstrayer@gmail.com>
2024-08-19 23:32:23 -0600

    Reorganize PlayerHeader file structure and place inline styling in separate file

--- frontend/src/components/PlayerHeader/PlayerHeader.tsx
+++ frontend/src/components/PlayerHeader/PlayerHeader.tsx
@@ -1,7 +1,8 @@
 import { Avatar, Box } from "@mui/material";
 import React from "react";
-import { RankedData } from "../pages/PlayerProfile";
-import RankedDataDisplay from "./RankedDataDisplay";
+import { RankedData } from "../../pages/PlayerProfile";
+import RankedDataDisplay from "../RankedDataDisplay";
+import "./PlayerHeader.css";
 
 interface PlayerHeaderProps {
   id: string;
@@ -18,19 +19,18 @@ const PlayerHeader: React.FC<PlayerHeaderProps> = (p) => {
   return (
     <Box display={"flex"} m={"5vw"}>
       <Avatar
+        className="profile-icon"
         alt="Profile Icon"
         src={`http://ddragon.leagueoflegends.com/cdn/14.14.1/img/profileicon/${p.profileIconId}.png`}
-        sx={{ width: "15vw", height: "15vw" }}
-        style={{ borderRadius: "1vw", objectFit: "cover" }}
       />
-      <Box display={"flex"} ml={"2vw"} flexDirection={"column"}>
-        <Box display={"flex"} fontSize={"3vw"}>
-          <Box fontWeight={"bold"}>{p.name}</Box>
-          <Box color="black" ml={"1vw"}>{`#${p.tagLine}`}</Box>
+      <Box className="container">
+        <Box className="first-header-container">
+          <Box className="player-name">{p.name}</Box>
+          <Box className="tag-line">{`#${p.tagLine}`}</Box>
         </Box>
-        <Box display={"flex"} fontSize={"1.5vw"}>
+        <Box className="second-header-container">
           <Box>Level {p.summonerLevel}</Box>
-          <Box ml={"1vw"}>{p.region}</Box>
+          <Box className="region">{p.region}</Box>
         </Box>
         <RankedDataDisplay soloData={p.soloData} flexData={p.flexData} />
       </Box>
--- frontend/src/pages/PlayerProfile.tsx
+++ frontend/src/pages/PlayerProfile.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import PlayerHeader from "../components/PlayerHeader";
+import PlayerHeader from "../components/PlayerHeader/PlayerHeader";
 
 interface PlayerData {
   id: string;
