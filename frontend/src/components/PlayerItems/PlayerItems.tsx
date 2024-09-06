320c87ae4ea5eb68758725f16c03a75461a4b5db
Chase <brendanstrayer@gmail.com>
2024-09-05 23:25:28 -0600

    Use new ItemContainer component within PlayerItems component

--- frontend/src/components/PlayerItems/PlayerItems.tsx
+++ frontend/src/components/PlayerItems/PlayerItems.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { ParticipantDto } from "../../types/ParticipantDto";
-import { Avatar, Box } from "@mui/material";
+import { Box } from "@mui/material";
 import "./PlayerItems.css";
+import ItemContainer from "../ItemContainer/ItemContainer";
 
 interface PlayerItemsProps {
     player: ParticipantDto;
@@ -10,68 +11,13 @@ interface PlayerItemsProps {
 const PlayerItems = (props: PlayerItemsProps) => {
   return (
     <Box className="main-player-items-container">
-      {props.player.item0 ? (
-        <Avatar
-          src={`https://ddragon.leagueoflegends.com/cdn/14.16.1/img/item/${props.player.item0}.png`}
-          style={{ width: "3vw", height: "3vw", borderRadius: "0.5vw" }}
-        />
-      ) : (
-        <></>
-      )}
-
-      {props.player.item1 ? (
-        <Avatar
-          src={`https://ddragon.leagueoflegends.com/cdn/14.16.1/img/item/${props.player.item1}.png`}
-          style={{ width: "3vw", height: "3vw", borderRadius: "0.5vw" }}
-        />
-      ) : (
-        <></>
-      )}
-
-      {props.player.item2 ? (
-        <Avatar
-          src={`https://ddragon.leagueoflegends.com/cdn/14.16.1/img/item/${props.player.item2}.png`}
-          style={{ width: "3vw", height: "3vw", borderRadius: "0.5vw" }}
-        />
-      ) : (
-        <></>
-      )}
-
-      {props.player.item3 ? (
-        <Avatar
-          src={`https://ddragon.leagueoflegends.com/cdn/14.16.1/img/item/${props.player.item3}.png`}
-          style={{ width: "3vw", height: "3vw", borderRadius: "0.5vw" }}
-        />
-      ) : (
-        <></>
-      )}
-
-      {props.player.item4 ? (
-        <Avatar
-          src={`https://ddragon.leagueoflegends.com/cdn/14.16.1/img/item/${props.player.item4}.png`}
-          style={{ width: "3vw", height: "3vw", borderRadius: "0.5vw" }}
-        />
-      ) : (
-        <></>
-      )}
-
-      {props.player.item5 ? (
-        <Avatar
-          src={`https://ddragon.leagueoflegends.com/cdn/14.16.1/img/item/${props.player.item5}.png`}
-          style={{ width: "3vw", height: "3vw", borderRadius: "0.5vw" }}
-        />
-      ) : (
-        <></>
-      )}
-
-      {props.player.item6 ? (
-        <Avatar
-          src={`https://ddragon.leagueoflegends.com/cdn/14.16.1/img/item/${props.player.item6}.png`}
-          style={{ width: "3vw", height: "3vw", borderRadius: "0.5vw" }}
-        />
-      ) : (
-        <></>
-      )}
+        <ItemContainer id={props.player.item0}/>
+        <ItemContainer id={props.player.item1}/>
+        <ItemContainer id={props.player.item2}/>
+        <ItemContainer id={props.player.item3}/>
+        <ItemContainer id={props.player.item4}/>
+        <ItemContainer id={props.player.item5}/>
+        <ItemContainer id={props.player.item6}/>
     </Box>
   );
 };
