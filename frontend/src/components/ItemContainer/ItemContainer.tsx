a3876947b9fb279f126f2cc55c9c502a0ad34af6
Chase <brendanstrayer@gmail.com>
2024-09-05 23:24:50 -0600

    Create new ItemContainer component

---
+++ frontend/src/components/ItemContainer/ItemContainer.tsx
@@ -0,0 +1,22 @@
+import { Avatar } from "@mui/material";
+import React from "react";
+import "./ItemContainer.css";
+
+interface ItemContainerProps {
+  id?: number;
+}
+
+const ItemContainer = (props: ItemContainerProps) => {
+  if (props.id) {
+    return (
+      <Avatar
+        src={`https://ddragon.leagueoflegends.com/cdn/14.16.1/img/item/${props.id}.png`}
+        style={{ width: "3vw", height: "3vw", borderRadius: "0.5vw" }}
+        className="item-container"
+      />
+    );
+  }
+  return <></>;
+};
+
+export default ItemContainer;
