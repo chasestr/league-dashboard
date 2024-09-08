3931a8788927fb0c74bf04d5fe9295b517371e16
Chase <brendanstrayer@gmail.com>
2024-09-07 22:16:01 -0600

    Add region field to PlayerData type

--- frontend/src/types/PlayerData.ts
+++ frontend/src/types/PlayerData.ts
@@ -5,6 +5,7 @@ export interface PlayerData {
   id: string;
   name: string;
   tagline: string;
+  region: string;
   profileIconId: number;
   summonerLevel: number;
   soloData: Array<RankedData>;
