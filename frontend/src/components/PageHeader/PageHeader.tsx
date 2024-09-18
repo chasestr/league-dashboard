ea3b13e15b0d6c4792ca8e65608a01d83cf9bd2e
Chase <brendanstrayer@gmail.com>
2024-09-17 23:07:16 -0600

    Add SearchBar component to PageHeader

--- frontend/src/components/PageHeader/PageHeader.tsx
+++ frontend/src/components/PageHeader/PageHeader.tsx
@@ -1,13 +1,15 @@
 import { Box } from "@mui/material";
 import React from "react";
 import "./PageHeader.css";
+import SearchBar from "../SearchBar/SearchBar";
 
 const PageHeader = () => {
   return (
     <Box className="page-header-container">
       <a href="/" className="page-header-text">
-        League Dashboard
+        LoLDash
       </a>
+      <SearchBar/>
     </Box>
   );
 };
